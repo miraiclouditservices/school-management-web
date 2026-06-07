a2fcdffe563844718bdb4869994de3a62b4e2fc7
Naveen-byte-gif <tungananaveenjob18@gmail.com>
2026-06-07 23:58:23 +0530

    URL change again fresh URL

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://school-management-pkej.onrender.com/api',
   },
 };
 module.exports = nextConfig;
